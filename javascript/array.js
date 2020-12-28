86a8d31197098f570e4313b799138b154748ba5a
Mohammed <adamnahar3020@gmail.com>
2020-12-28 11:46:05 +0100

    array

---
+++ javascript/array.js
@@ -0,0 +1,14 @@
+var friends = [
+    "Ali",
+    "Adam",
+    "John",
+    "Simon",
+    "Gamer",
+];
+console.log(friends);
+//sort
+friends.sort();
+console.log(friends);
+//reverse
+friends.reverse();
+console.log(friends);
