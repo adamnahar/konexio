261c44946e46ddfc594cf1ad0a5882a8792784d4
Mohammed <adamnahar3020@gmail.com>
2021-01-04 15:17:42 +0100

    mon premier version de jour 7

---
+++ javascript/fs.js
@@ -0,0 +1,16 @@
+// file system
+
+var fs = require("fs");
+fs.readFile("./jour7.txt", function(error, data){
+    console.log(data.toString());
+})
+
+//map double
+
+// map names
+
+//04 - Filter Numbers
+
+//05 filter event
+
+//06 cakes
