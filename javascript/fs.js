2e3df95eee3f4171a04c0ae59442ce8829fedaf5
Mohammed <adamnahar3020@gmail.com>
2021-01-05 09:22:28 +0100

    mon version de fs.js et jour7.txt

--- javascript/fs.js
+++ javascript/fs.js
@@ -1,16 +1,49 @@
-// file system
+//01 file system
 
 var fs = require("fs");
 fs.readFile("./jour7.txt", function(error, data){
     console.log(data.toString());
 })
 
-//map double
+//02 map double
 
-// map names
+var x = [1, 2, 3, 4, 5];
+var doubles = x.map(function(num) {
+  return num * 2;
+});
+console.log(doubles);
+
+//03 map Names
+
+//03  map names
+var longName = [
+	{
+		firstName: "Jane",
+		lastName: "Doe"
+	},
+	{
+		firstName: "John",
+		lastName: "Smith",
+	}
+]
+var shortName = shortName.map(function(name){
+
+
+[
+	{
+		name: "Jane Doe"
+	},
+	{
+		name: "John Smith"
+	}
+]
+ console.log(shortName);
+
 
 //04 - Filter Numbers
 
+
 //05 filter event
 
-//06 cakes
+//06 Cake
+
