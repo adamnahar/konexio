b7da67fe605efd1e963b2570218ddfc79abb5ff7
Mohammed <adamnahar3020@gmail.com>
2021-01-05 15:49:35 +0100

    jour8 ex 2

--- javascript/jour8.js
+++ javascript/jour8.js
@@ -1,6 +1,14 @@
+// 01 - countries
 var request = require("request");
 request.get("https://restcountries.eu/rest/v1/all" , function(error, response, body){
     var all=JSON.parse(body);
     console.log(all);
     var countriesNames = []
 })
+
+// 02 - chuck norris
+request.get("https://api.chucknorris.io/jokes/random", function(error, response, body){
+    var getFact= JSON.parse(body);
+    console.log(getFact.value);
+})
+// 03 -pokemon
