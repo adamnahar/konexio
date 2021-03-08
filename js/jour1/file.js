8d58b3d9a98b2b1e5fcbf72d982d1d13dd809538
adamnahar <adamnahar3020@gmail.com>
2021-03-08 17:16:50 +0100

    jour1

---
+++ js/jour1/file.js
@@ -0,0 +1,54 @@
+console.log("hello world");
+//var name = value
+var name = "Nahar";
+
+console.log(name);
+
+// string
+var test = "My name is Adam";
+console.log(test);
+
+// Concatenation
+
+var name = "Adam";
+console.log('Nice to meet you ' + name);
+
+// String Length
+var test = "I'm very long !";
+console.log(test.length);
+
+var food = "croissant is meh";
+food = food.replace("meh", "so good");
+console.log(food);
+
+// Up and Down
+
+var basic = "This is Cool";
+var basicUp = basic.toUpperCase();
+var basicDown = basic.toLocaleLowerCase();
+
+console.log(basic);
+console.log(basicUp);
+console.log(basicDown);
+
+//  Split
+
+var word = "banana";
+var letters = word.split("");
+console.log(letters);
+
+//  Template
+
+//var age = "10";
+//var template = `I'm ${age} years old`;
+//console.log(template);
+
+var template2 = "I'm " + age + " years old";
+console.log(template2);
+
+
+
+
+
+
+
