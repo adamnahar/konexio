1be084748535997a4fffa7f90a5529e5084fe437
adamnahar <adamnahar3020@gmail.com>
2021-03-09 15:51:38 +0100

    jour2 number,convert,round,arithmetique

---
+++ js/jour1/jour2.js
@@ -0,0 +1,23 @@
+//number
+var integer = 102;
+console.log(integer);
+
+var float = 13.9;
+console.log(float);
+
+//convert
+
+var basic =34.
+var stringified =basic.toString();
+console.log("convert :", stringified.length);
+
+//round
+var num = 1.5;
+var rounded = Math.round(num);
+console.log("Round :", rounded);
+
+//arithemetique
+var test = 12;
+var bis =5;
+console.log("Arithmétique" , test +bis, test - bis,test * bis,test / bis, test ** bis, test % bis);
+
